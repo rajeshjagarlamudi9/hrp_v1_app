4801e7e7da6dc7deb742ab0dc17ded0046acd67d
Rajesh J <jagarlamudirajesh34@gmail.com>
2016-10-20 20:08:47 +0530

    new pojo object UserServices

---
+++ HRP_App_Version1/src/main/java/com/hrp/model/UserServices.java
@@ -0,0 +1,122 @@
+package com.hrp.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_services")
+public class UserServices {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", updatable = false, insertable = false)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "service_id", updatable = false, insertable = false)
+	private Services services;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "role_id", updatable = false, insertable = false)
+	private Role role;
+
+	@Column(name = "created_date", updatable = false)
+	private Date createdDate;
+
+	@Column(name = "updated_date")
+	private Date updatedDate;
+
+	@Column(name = "deleted_yn")
+	private Boolean deletedYn;
+
+	public UserServices() {
+		super();
+	}
+
+	public UserServices(UserServices userServices) {
+		super();
+		this.id = userServices.id;
+		this.user = userServices.user;
+		this.role = userServices.role;
+		this.services = userServices.services;
+		this.createdDate = userServices.createdDate;
+		this.updatedDate = userServices.updatedDate;
+		this.deletedYn = userServices.deletedYn;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Services getServices() {
+		return services;
+	}
+
+	public void setServices(Services services) {
+		this.services = services;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	public Boolean getDeletedYn() {
+		return deletedYn;
+	}
+
+	public void setDeletedYn(Boolean deletedYn) {
+		this.deletedYn = deletedYn;
+	}
+
+	@Override
+	public String toString() {
+		return "UserServices [id=" + id + ", user=" + user + ", services=" + services + ", role=" + role
+				+ ", createdDate=" + createdDate + ", updatedDate=" + updatedDate + ", deletedYn=" + deletedYn + "]";
+	}
+
+}
